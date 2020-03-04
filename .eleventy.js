aea70d94b5bab35a54763ef6d48fcfbccdf0744d
Zee <zee@zinc.coop>
2020-03-04 12:27:40 -0800

    Initialize eleventy (#1)
    We still have copy to update,  and we would  prefer for this to be a single page that renders plain-jane HTML; but also can be accessed at <domain>/specific-location-on-single-page
    without making it into a godforsaken SPA.
    
    We used [this template](https://github.com/philhawksworth/eleventyone) as a reference for how to make eleventy work but I think it was too big of a step; so I got confused.
    
    Co-authored-by: Tom Lee <user512@users.noreply.github.com>

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(config) {
+  // A useful way to reference the context we are runing eleventy in
+  let env = process.env.ELEVENTY_ENV;
+
+  // Layout aliases can make templates more portable
+  config.addLayoutAlias('default', 'layouts/base.njk');
+
+  // pass some assets right through
+  config.addPassthroughCopy("./src/site/images");
+
+  return {
+    dir: {
+      input: "src/site",
+      output: "dist",
+      data: `_data/${env}`
+    },
+    templateFormats: ["html", "njk", "md", "11ty.js"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: true
+  }
+}
