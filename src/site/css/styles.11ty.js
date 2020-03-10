c66198259cdfced7fac917d71443ebf0f9cdcf82
Tom Lee <user512@users.noreply.github.com>
2020-03-10 11:21:51 -0700

    Setup tailwind css and ensure old cc-icon works

--- src/site/css/styles.11ty.js
+++ src/site/css/styles.11ty.js
@@ -4,7 +4,7 @@ const postcss = require('postcss');
 
 // the file name as an entry point for postcss compilation
 // also used to define the output filename in our output /css folder.
-const fileName = "styles.css";
+const fileName = "main.css";
 
 module.exports = class {
   async data () {
@@ -18,12 +18,13 @@ module.exports = class {
 
   async render ({ rawCss, rawFilepath }) {
     return await postcss([
-      // require('postcss-comment'),
       require('precss'),
       require('postcss-import'),
       require('postcss-mixins'),
       require('postcss-color-mix'),
-      require('cssnano')
+      require('cssnano'),
+      require('tailwindcss'),
+      require('autoprefixer'),
     ])
     .process(rawCss, { from: rawFilepath })
     .then(result => result.css);
